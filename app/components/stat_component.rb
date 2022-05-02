38196bca33c78325d3e5fff2f5f7ae9fcec982f5
Justine Saupin <sau.justine@gmail.com>
2022-05-02 11:20:20 +0200

    component_card css

--- app/components/stat_component.rb
+++ app/components/stat_component.rb
@@ -12,6 +12,6 @@ class StatComponent < ViewComponent::Base
   end
 
   def card_logo
-    image_tag(logo, class:"w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg")
+    image_tag(logo)
   end
 end
